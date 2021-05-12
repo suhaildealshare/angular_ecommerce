0da0a072ea7724362f3357b0d9cd8cb555b57f3e
suhail_khan28 <suhail.khan@walletdoc.com>
2021-05-12 15:52:04 +0530

    Shopping list and other component added with layout

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.scss']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a description of the test recipe', 'https://live.staticflickr.com/5702/31078600701_812bd6aa88_b.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
-
+ingredients: Ingredients[] =[
+  new Ingredients('Apple', 5),
+  new Ingredients('Tomato', 10)
+];
   constructor() { }
 
   ngOnInit(): void {
